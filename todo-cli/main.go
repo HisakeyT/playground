bf0341d09255517a742c6c310e3576dd0b4d36ed
hisakeyT <110322156+HisakeyT@users.noreply.github.com>
2026-06-27 06:16:36 +0900

    feat: #2 command error

--- todo-cli/main.go
+++ todo-cli/main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args
+	if len(args) < 2 {
+		printUsage()
+		return
+	}
+
+	switch args[1] {
+	case "list":
+		fmt.Println("Listing todos...")
+	case "add":
+		if len(args) < 3 {
+			fmt.Println("titleがありません")
+			return
+		}
+		title := args[2]
+		fmt.Printf("Adding todo: %s\n", title)
+	default:
+		fmt.Printf("Unknown command: %s\n\n", args[1])
+		printUsage()
+	}
+}
+
+func printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("  go run . list")
+	fmt.Println("  go run . add <title>")
 }

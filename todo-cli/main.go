d9819a4c070765cfb99dc5124f0d4e6c5d24c10a
hisakeyT <110322156+HisakeyT@users.noreply.github.com>
2026-06-28 13:32:23 +0900

    feat: step3 type is added

--- todo-cli/main.go
+++ todo-cli/main.go
@@ -2,34 +2,41 @@ package main
 
 import (
 	"fmt"
-	"os"
+	// "os"
 )
 
 func main() {
-	args := os.Args
-	if len(args) < 2 {
-		printUsage()
-		return
+	todo := Todo{
+		ID:    1,
+		Title: "Buy groceries",
+		Done:  false,
 	}
 
-	switch args[1] {
-	case "list":
-		fmt.Println("Listing todos...")
-	case "add":
-		if len(args) < 3 {
-			fmt.Println("titleがありません")
-			return
-		}
-		title := args[2]
-		fmt.Printf("Adding todo: %s\n", title)
-	default:
-		fmt.Printf("Unknown command: %s\n\n", args[1])
-		printUsage()
-	}
-}
-
-func printUsage() {
-	fmt.Println("Usage:")
-	fmt.Println("  go run . list")
-	fmt.Println("  go run . add <title>")
+	fmt.Println(todo)
+	//		args := os.Args
+	//		if len(args) < 2 {
+	//			printUsage()
+	//			return
+	//		}
+	//
+	//		switch args[1] {
+	//		case "list":
+	//			fmt.Println("Listing todos...")
+	//		case "add":
+	//			if len(args) < 3 {
+	//				fmt.Println("titleがありません")
+	//				return
+	//			}
+	//			title := args[2]
+	//			fmt.Printf("Adding todo: %s\n", title)
+	//		default:
+	//			fmt.Printf("Unknown command: %s\n\n", args[1])
+	//			printUsage()
+	//		}
+	//	}
+	//
+	//	func printUsage() {
+	//		fmt.Println("Usage:")
+	//		fmt.Println("  go run . list")
+	//		fmt.Println("  go run . add <title>")
 }
